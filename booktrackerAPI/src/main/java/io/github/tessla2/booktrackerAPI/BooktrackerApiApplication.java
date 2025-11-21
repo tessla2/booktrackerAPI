c8c28ad31648bfbaa0b704c1fcbfc101875ded5c
tainara.luza <tessdev3@gmail.com>
2025-11-21 17:28:26 +0000

    Initial commit

---
+++ booktrackerAPI/src/main/java/io/github/tessla2/booktrackerAPI/BooktrackerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.tessla2.booktrackerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tessla2/booktrackerAPI/BooktrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tessla2.booktrackerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
